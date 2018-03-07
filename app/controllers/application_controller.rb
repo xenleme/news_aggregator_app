7c2c4579a46aa98cb32c5d507c7b5fb462185138
xenleme <xenleme@gmail.com>
2018-03-07 13:22:13 +0300

    added possibility to leave comments only authorized users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_user_location!, if: :storable_location?
+  before_action :authenticate_user!
+
+  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
   end
 end
