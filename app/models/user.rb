4b3f9ce31b38cc01980dd2a863aca624cda41b18
xenleme <xenleme@gmail.com>
2018-03-08 19:55:22 +0300

    add user avatar, few style fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :links
+
+  has_attached_file :avatar, :styles => { :medium => "200x200>", :thumb => "20x20>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305132225) do
+ActiveRecord::Schema.define(version: 20180308104056) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "link_id"
@@ -45,6 +45,10 @@ ActiveRecord::Schema.define(version: 20180305132225) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
