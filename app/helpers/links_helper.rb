49252313205eb67295714221b8b2859d02232ac3
xenleme <xenleme@gmail.com>
2018-03-10 11:38:51 +0300

    add links helpers

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,9 @@
 module LinksHelper
+  def truncated_link_title(link)
+    link_to truncate(link.title, length: 70), link
+  end
+
+  def truncated_url_title(link)
+    link_to truncate(link.url, length: 30), link.url, target: :blank
+  end
 end
